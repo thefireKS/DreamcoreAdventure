971fea6368118e2995712ac394a15869ae970d37
Nikita <84981163+MorgoWizard@users.noreply.github.com>
2023-12-04 02:38:00 +0300

    Add music manager and camera off

---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Level Data")]
+public class LevelData : ScriptableObject
+{
+    public AudioClip Embient;
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MusicManager))]
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager instance;
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+    [SerializeField] private AudioSource audioSource;
+
+    public void ChangeMusic(AudioClip audioClip)
+    {
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+}
